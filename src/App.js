2ff0d095435c6a3e0945f1c5fad694f624985e09
surtich <surtich@gmail.com>
2021-12-13 13:25:35 +0100

    Using object expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-function App() {
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
 
-  const title = 'React';
+function App() {
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text">
+      </input>
     </div>
   )
 }

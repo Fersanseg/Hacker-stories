e1024bb1ab837b354700a76f901e5679f948517a
surtich <surtich@gmail.com>
2021-12-13 13:27:34 +0100

    Using function expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
+function greeting(title) {
+  return 'Hello ' + title;
+}
 
 function App() {
 
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>{greeting('React')}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text">

295d6a645c2919ebb66007834277b7bb624a3d7e
surtich <surtich@gmail.com>
2021-12-15 13:15:54 +0100

    Split components in files

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import List from "./List";
+import Search from "./Search";
+
+function App() {
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+
+      <hr />
+
+      <List />
+
+    </div>
+  )
+}
+
+export default App;

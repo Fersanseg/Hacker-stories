277ea26b9cffdbc7e6e5b229fcf4968675b4bd2f
surtich <surtich@gmail.com>
2021-12-15 12:34:56 +0100

    Using lists

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-function Greeting() {
-  return "Hello";
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
 
-
   return (
     <div>
-      <h1><Greeting /></h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text">
-      </input>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {
+          list.map(function (item) {
+            return <li key={item.objectID}>{item.title}</li>
+          })
+        }
+      </ul>
+
     </div>
   )
 }

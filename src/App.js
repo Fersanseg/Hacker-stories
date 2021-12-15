eb94ffe38bf6dfaf299760209e7024cabfd3bae0
surtich <surtich@gmail.com>
2021-12-15 11:52:25 +0100

    Using function of functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 function greeting(title) {
-  return 'Hello ' + title;
+  return function () {
+    return <div>Hello {title}</div>;
+  }
 }
+
+const ReactGreet = greeting('React');
 
 function App() {
 
+
   return (
     <div>
-      <h1>{greeting('React')}</h1>
+      <h1><ReactGreet /></h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text">

ffeeb9119e8ec01d53ec34ce7b4aeb1ffbfdecad
surtich <surtich@gmail.com>
2021-12-15 13:02:39 +0100

    Using nested components(ii)

--- src/App.js
+++ src/App.js
@@ -36,14 +36,22 @@ function List() {
   );
 }
 
+function Search() {
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </>
+  );
+}
+
 function App() {
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 

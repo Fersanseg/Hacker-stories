ce7d8046b6b9b93e38234d19c86dc6f0475c672e
surtich <surtich@gmail.com>
2021-12-20 12:03:12 +0100

    using React router

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { Outlet, Link } from "react-router-dom";
 import List from "./List";
 import Search from "./Search";
 
@@ -6,13 +7,19 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
       <Search />
-
       <hr />
-
       <List />
-
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   )
 }
